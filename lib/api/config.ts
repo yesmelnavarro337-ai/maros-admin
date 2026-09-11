cc0c138500162ab6067c2bc683c29b5feaed40c4
yesmeljose337-arch <yesmeljose337@gmail.com>
2026-09-11 07:59:52 -0500

    modificacion

--- lib/api/config.ts
+++ lib/api/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.API_URL ?? "http://localhost:5072";
+export const API_URL = process.env.API_URL ?? "https://maros-backend-pjvy.onrender.com";
 export const AUTH_COOKIE_NAME = "maros_admin_token";
